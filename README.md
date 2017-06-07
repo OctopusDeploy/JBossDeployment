a0b70ae62d425d96037e7963773007b972585177
Matthew Casperson <matthewcasperson@gmail.com>
2017-06-07 11:15:23 +1000

    Working through domain deployment scenarios

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# JBossDeployment
+# JBoss Deployment
+
+This repo contains scripts that can be used to deploy to a WildFly / EAP server
+via the cli.
+
+These scripts have been designed to allow Octopus Deploy to deploy a release, and 
+make the following assumptions:
+
+1.  Each deployment will always overwrite an existing one.
+2.  Server groups that are not specified in the command will use the new deployment (because of 1, we are always 
+    updating deployments). The enabled or disabled state for unspecified server groups remains unchanged.
+
+## Tests
+
+### Standalone
+Deploy to standalone
+Deploy to standalone disabled
+Redeploy to standalone while currently enabled
+Redeploy to standalone disabled while currently enabled
+Redeploy to standalone while currently disabled
+Redeploy to standalone disabled while currently disabled
+
+### Domain Controller
+Deploy to domain and server group while deployment does not exist *
+Redeploy to domain and server group while currently exists and unassigned *
+Redeploy to domain and server group while currently exists and assigned and enabled *
+Redeploy to domain and server group while currently exists and assigned and disabled *
+
+Deploy to domain and server group disabled while deployment does not exist *
+Redeploy to domain and server group disabled while currently exists and unassigned 8
+Redeploy to domain and server group disabled while currently exists and assigned and enabled *
+Redeploy to domain and server group disabled while currently exists and assigned and disabled *
+
+Deploy to domain while deployment does not exist ?
+Redeploy to domain while currently exists and unassigned
+Redeploy to domain while currently exists and assigned and disabled
+Redeploy to domain while currently exists and assigned and enabled
+
+Deploy to domain disabled while deployment does not exist
+Redeploy to domain disabled while currently exists and unassigned
+Redeploy to domain disabled while currently exists and assigned and disabled
+Redeploy to domain disabled while currently exists and assigned and enabled
--- deploy-app.groovy
+++ deploy-app.groovy
@@ -2,7 +2,7 @@
 
 @Grab(group='org.wildfly.core', module='wildfly-embedded', version='2.2.1.Final')
 @Grab(group='org.wildfly.security', module='wildfly-security-manager', version='1.1.2.Final')
-@Grab(group='org.wildfly.core', module='wildfly-cli', version='2.2.1.Final')
+@Grab(group='org.wildfly.core', module='wildfly-cli', version='3.0.0.Beta23')
 import org.jboss.as.cli.scriptsupport.*
 
 @Grab(group='org.springframework.retry', module='spring-retry', version='1.2.0.RELEASE')
@@ -10,6 +10,12 @@ import org.springframework.retry.*
 import org.springframework.retry.support.*
 import org.springframework.retry.backoff.*
 import org.springframework.retry.policy.*
+
+@Grab(group='commons-io', module='commons-io', version='2.5')
+import org.apache.commons.io.*
+
+@Grab(group='com.google.guava', module='guava', version='22.0')
+import com.google.common.base.Splitter
 
 final DEFAULT_HOST = "localhost"
 final DEFAULT_PORT = 9990
@@ -25,6 +31,7 @@ cli.with {
     a longOpt: 'application', args: 1, argName: 'path to artifact', 'Application to be deployed'
     n longOpt: 'name', args: 1, argName: 'application name', 'Application name'
     e longOpt: 'disabled', 'Disable the application'
+    s longOpt: 'serverGroup', args: 1, argName: 'server group', 'Server group to deploy to'
 }
 
 def options = cli.parse(args)
@@ -38,7 +45,7 @@ if (options.h) {
     return
 }
 
-cli = CLI.newInstance()
+def jbossCli = CLI.newInstance()
 
 /*
    Build up a retry template
@@ -57,7 +64,10 @@ retryTemplate.setBackOffPolicy(backOffPolicy)
 retryTemplate.execute(new RetryCallback<Void, Exception>() {
     @Override
     Void doWithRetry(RetryContext context) throws Exception {
-        cli.connect(
+
+        println("Attempting to connect. Retry count: ${context.retryCount}")
+
+        jbossCli.connect(
                 options.controller ?: DEFAULT_HOST,
                 options.port ?: DEFAULT_PORT,
                 options.user,
@@ -69,20 +79,97 @@ retryTemplate.execute(new RetryCallback<Void, Exception>() {
 /*
     Upload the package
  */
-def disabled = (options.disabled ?: false) ? "--disabled" : ""
-def name = options.name ? "--name=${options.name}" : ""
+def packageName = FilenameUtils.getName(options.application)
+def name = options.name ? "--name=${options.name}" : "--name=${packageName}"
 
-if (cli.getCommandContext().isDomainMode()) {
+if (jbossCli.getCommandContext().isDomainMode()) {
 
-}
+    /*
+        Upload the new content
+     */
+    retryTemplate.execute(new RetryCallback<CLI.Result, Exception>() {
+        @Override
+        CLI.Result doWithRetry(RetryContext context) throws Exception {
+            println("Attempt ${context.retryCount + 1} to upload package.")
 
-retryTemplate.execute(new RetryCallback<CLI.Result, Exception>() {
-    @Override
-    CLI.Result doWithRetry(RetryContext context) throws Exception {
-        def result = cli.cmd("deploy --force ${disabled} ${name} ${options.application}")
-        if (!result.success) {
-            throw new Exception("Failed to upload package")
+            def result = jbossCli.cmd("deploy --force ${name} ${options.application}")
+            if (!result.success) {
+                throw new Exception("Failed to upload package")
+            }
+            return result
         }
-        return result
+    })
+
+    /*
+        If the deployment has instructions for a server group, complete them
+     */
+    if (options.serverGroup) {
+
+        Splitter.on(',')
+                .trimResults()
+                .omitEmptyStrings()
+                .split(options.serverGroup)
+                .each {
+                    /*
+                        Add the deployment to the server group in a disabled state
+                     */
+                    retryTemplate.execute(new RetryCallback<Void, Exception>() {
+                        @Override
+                        Void doWithRetry(RetryContext context) throws Exception {
+                            println("Attempt ${context.retryCount + 1} to look up existing package.")
+
+                            def readResourceResult = jbossCli.cmd("/server-group=${it}/deployment=${packageName}:read-resource")
+                            if (!readResourceResult.success) {
+                                println("Attempt ${context.retryCount + 1} to add package.")
+
+                                def result = jbossCli.cmd("/server-group=${it}/deployment=${packageName}:add")
+                                if (!result.success) {
+                                    throw new Exception("Failed to add package")
+                                }
+                            }
+
+                            return null
+                        }
+                    })
+
+                    /*
+                        Enable or disable the deployment
+                     */
+                    retryTemplate.execute(new RetryCallback<CLI.Result, Exception>() {
+                        @Override
+                        CLI.Result doWithRetry(RetryContext context) throws Exception {
+                            def action = options.disabled ? "undeploy" : "deploy"
+                            println("Attempt ${context.retryCount + 1} to ${action} package.")
+
+                            def result = jbossCli.cmd("/server-group=${it}/deployment=${packageName}:${action}")
+                            if (!result.success) {
+                                throw new Exception("Failed to deploy package")
+                            }
+                            return result
+                        }
+                    })
+
+                }
     }
-})
+
+
+} else {
+    if (options.serverGroup) {
+        println("The serverGroups option is only valid when deploying to a domain")
+    }
+
+    def disabled = (options.disabled ?: false) ? "--disabled" : ""
+
+    retryTemplate.execute(new RetryCallback<CLI.Result, Exception>() {
+        @Override
+        CLI.Result doWithRetry(RetryContext context) throws Exception {
+            println("Attempting to upload package. Retry count: ${context.retryCount}")
+
+            def result = jbossCli.cmd("deploy ${disabled} ${name} ${options.application}")
+            if (!result.success) {
+                throw new Exception("Failed to upload package")
+            }
+            return result
+        }
+    })
+}
