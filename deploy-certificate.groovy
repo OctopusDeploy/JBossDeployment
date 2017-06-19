4c9da253c47206ba5a2be86d0181843923e1881f
Matthew Casperson <matthew.casperson@octopus.com>
2017-06-20 06:48:12 +1000

    Working on certificate deployment

--- deploy-certificate.groovy
+++ deploy-certificate.groovy
@@ -250,7 +250,7 @@ def configureManagement = { host ->
         CLI.Result doWithRetry(RetryContext context) throws Exception {
             println("Attempt ${context.retryCount + 1} to change management socket binding for ${host}.")
 
-            def socketBindingResult = jbossCli.cmd("${host}/core-service=management/management-interface=http-interface:write-attribute(" +
+            def socketBindingResult = jbossCli.cmd("${hostPrefix}/core-service=management/management-interface=http-interface:write-attribute(" +
                     "name=secure-socket-binding, " +
                     "value=management-https")
             if (!socketBindingResult.success) {
